60ee874ec2b76f422217571078718adb5c2079e7
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2014-12-13 12:41:12 +0000

    conf.d

--- manifests/process.pp
+++ manifests/process.pp
@@ -1,6 +1,6 @@
 define god::process ($name,$pidfile,$start_command, $stop_command, $restart_command) {
   file { "god-$name": 
-    path => "/etc/god/$name.god",
+    path => "/etc/god/conf.d/$name.god",
     content => template("god/sample.god.erb"),
     mode => "0644",
     require => Class["god"]
