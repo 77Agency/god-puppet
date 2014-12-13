397f96c4209a90d33166d2985046171fd0074396
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2014-12-13 13:48:45 +0000

    make commands optional

--- manifests/process.pp
+++ manifests/process.pp
@@ -1,4 +1,4 @@
-define god::process ($name,$pidfile,$start_command, $stop_command, $restart_command) {
+define god::process ($name,$pidfile = undef,$start_command, $stop_command = undef, $restart_command = undef) {
   file { "god-$name": 
     path => "/etc/god/conf.d/$name.god",
     content => template("god/sample.god.erb"),
