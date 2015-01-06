313108e30c56c46b71cdf895fb783e1777b60362
Andrea Maria Piana <a.piana@77agency.com>
2015-01-06 13:13:19 +0000

    allow raw data in process

--- manifests/process.pp
+++ manifests/process.pp
@@ -1,4 +1,4 @@
-define god::process ($name,$gid = undef, $uid = undef,$pidfile = undef,$start_command, $stop_command = undef, $restart_command = undef) {
+define god::process ($name,$gid = undef, $uid = undef,$pidfile = undef,$start_command, $stop_command = undef, $restart_command = undef, $raw_data = []) {
   file { "god-$name": 
     path => "/etc/god/conf.d/$name.god",
     content => template("god/sample.god.erb"),
